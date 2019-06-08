067e83e5a852da194fef8461aa4436aa2e795306
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-06-08 10:15:13 +0100

    Add border-radius and box-shaow to the card

--- src/components/LandingPage/card/card.js
+++ src/components/LandingPage/card/card.js
@@ -10,7 +10,9 @@ export default class Card extends Component {
 const FlexCard = styled.div`
 	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 	transition: 0.3s;
+	border-radius: 5px;
 
 	&::hover {
+		box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 	}
 `;
