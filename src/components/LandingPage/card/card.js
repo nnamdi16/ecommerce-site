726cdc5a5d4905eb396bb1d9df817a4166ea6266
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-06-09 03:20:59 +0100

    Update Card component

--- src/components/LandingPage/card/card.js
+++ src/components/LandingPage/card/card.js
@@ -5,7 +5,14 @@ export default class Card extends Component {
 	render() {
 		return (
 			<div className="card">
-				<FlexCard />
+				<FlexCard>
+					<header>New Arrival</header>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, odio voluptatum qui deserunt voluptate
+						fugit pariatur cum asperiores aut nemo laborum autem explicabo laboriosam reiciendis cumque consequuntur hic
+						tempora deleniti!
+					</p>
+				</FlexCard>
 			</div>
 		);
 	}
@@ -15,6 +22,8 @@ const FlexCard = styled.div`
 	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 	transition: 0.3s;
 	border-radius: 5px;
+	width: 60rem;
+	height: 30rem;
 
 	&::hover {
 		box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
